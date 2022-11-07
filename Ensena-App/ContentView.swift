75e88883dee2e5eda24ce4d0a31b2be41c9a8422
martnreh <martinbarrientos451@gmail.com>
2022-11-07 16:01:03 -0600

    Initial Commit

---
+++ Ensena-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ensena-App
+//
+//  Created by Adrian Martin Hernandez  on 07/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ensena-App/Ensena_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ensena_AppApp.swift
+//  Ensena-App
+//
+//  Created by Adrian Martin Hernandez  on 07/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Ensena_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
