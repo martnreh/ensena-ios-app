d85ffc6a1d98fc6823185abee18fec1bcf0aea8b
fazfaz7 <adrian.faz123@gmail.com>
2023-05-07 19:37:16 -0600

    Diccionario y más cursos

---
+++ Ensena-App/Views/InfoView.swift
@@ -0,0 +1,20 @@
+//
+//  InfoView.swift
+//  Ensena-App
+//
+//  Created by Adrian Faz on 07/05/23.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
